baa08a0a6469828f72c39c98c6d726e0dc954c0c
formloco <brock@formloco.com>
2022-02-18 08:40:06 -0700

    formloco env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -9,43 +9,43 @@ export const environment = {
   githubUrl: Formloco.githubUrl,
   kioskeEmail: Formloco.kioskeEmail,
   kioskePassword: Formloco.kioskePassword,
-  // apiUrl: Formloco.apiUrl,
-  // authUrl: Formloco.authUrl,
-  // formUrl: Formloco.formUrl,
-  // emailUrl: Formloco.emailUrl,
-  // assetUrl: Formloco.assetUrl,
-  // notificationUrl: Formloco.notificationUrl,
+  apiUrl: Formloco.apiUrl,
+  authUrl: Formloco.authUrl,
+  formUrl: Formloco.formUrl,
+  emailUrl: Formloco.emailUrl,
+  assetUrl: Formloco.assetUrl,
+  notificationUrl: Formloco.notificationUrl,
 
-  // messageUrl: Formloco.messageUrl,
-  // signinUrl: Formloco.signinUrl,
-  // redirectForgotPasswordUrl: Formloco.redirectForgotPasswordUrl,
+  messageUrl: Formloco.messageUrl,
+  signinUrl: Formloco.signinUrl,
+  redirectForgotPasswordUrl: Formloco.redirectForgotPasswordUrl,
 
-  // tenant: { email: Formloco.email, 
-  //           tenant_id: Formloco.tenant,
-  //           assetTenantId: 'a0642972-e528-4071-b756-e103e85cd9f4' // fixed-asset app
-  // },
-  // logo: Formloco.logo,
-  // idbName: Formloco.idbName,
-  // version: 'January 26, 2022',
-  // pin: Formloco.pin
-
-  apiUrl: Summit.apiUrl,
-  authUrl: Summit.authUrl,
-  formUrl: Summit.formUrl,
-  emailUrl: Summit.emailUrl,
-  assetUrl: Summit.assetUrl,
-  notificationUrl: Summit.notificationUrl,
-
-  messageUrl: Summit.messageUrl,
-  signinUrl: Summit.signinUrl,
-  redirectForgotPasswordUrl: Summit.redirectForgotPasswordUrl,
-
-  tenant: { email: Summit.email, 
-            tenant_id: Summit.tenant,
+  tenant: { email: Formloco.email, 
+            tenant_id: Formloco.tenant,
             assetTenantId: 'a0642972-e528-4071-b756-e103e85cd9f4' // fixed-asset app
   },
-  logo: Summit.logo,
-  idbName: Summit.idbName,
-  version: 'February 18, 2022',
-  pin: Summit.pin
+  logo: Formloco.logo,
+  idbName: Formloco.idbName,
+  version: 'January 26, 2022',
+  pin: Formloco.pin
+
+  // apiUrl: Summit.apiUrl,
+  // authUrl: Summit.authUrl,
+  // formUrl: Summit.formUrl,
+  // emailUrl: Summit.emailUrl,
+  // assetUrl: Summit.assetUrl,
+  // notificationUrl: Summit.notificationUrl,
+
+  // messageUrl: Summit.messageUrl,
+  // signinUrl: Summit.signinUrl,
+  // redirectForgotPasswordUrl: Summit.redirectForgotPasswordUrl,
+
+  // tenant: { email: Summit.email, 
+  //           tenant_id: Summit.tenant,
+  //           assetTenantId: 'a0642972-e528-4071-b756-e103e85cd9f4' // fixed-asset app
+  // },
+  // logo: Summit.logo,
+  // idbName: Summit.idbName,
+  // version: 'February 18, 2022',
+  // pin: Summit.pin
 }
