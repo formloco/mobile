a0d26b327cce9176d96388ccd83b0c8237f5499f
formloco <brock@formloco.com>
2022-05-23 13:10:35 -0600

    summit build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ import { Formloco, Summit, Rumzer } from '../app/state/app.state'
 
 export const environment = {
   production: true,
-  
+
   // kioske: true,
   // designUrl: Formloco.designKioskeUrl,
 
@@ -12,44 +12,45 @@ export const environment = {
   linkedinUrl: Formloco.linkedinUrl,
   githubUrl: Formloco.githubUrl,
 
-  apiUrl: Formloco.apiUrl,
-  authUrl: Formloco.authUrl,
-  formUrl: Formloco.formUrl,
-  homeUrl: Formloco.homeUrl,
-  emailUrl: Formloco.emailUrl,
-  assetUrl: Formloco.assetUrl,
-  notificationUrl: Formloco.notificationUrl,
+  // apiUrl: Formloco.apiUrl,
+  // authUrl: Formloco.authUrl,
+  // formUrl: Formloco.formUrl,
+  // homeUrl: Formloco.homeUrl,
+  // emailUrl: Formloco.emailUrl,
+  // assetUrl: Formloco.assetUrl,
+  // notificationUrl: Formloco.notificationUrl,
 
-  messageUrl: Formloco.messageUrl,
-  signinUrl: Formloco.signinUrl,
-  redirectForgotPasswordUrl: Formloco.redirectForgotPasswordUrl,
+  // messageUrl: Formloco.messageUrl,
+  // signinUrl: Formloco.signinUrl,
+  // redirectForgotPasswordUrl: Formloco.redirectForgotPasswordUrl,
 
-  tenant: { 
-    email: Formloco.email, 
-    tenant_id: Formloco.tenant
-  },
-  logo: Formloco.logo,
-  idbName: Formloco.idbName,
-  version: Formloco.version,
-  pin: Formloco.pin
-
-  // apiUrl: Summit.apiUrl,
-  // authUrl: Summit.authUrl,
-  // formUrl: Summit.formUrl,
-  // emailUrl: Summit.emailUrl,
-  // assetUrl: Summit.assetUrl,
-  // notificationUrl: Summit.notificationUrl,
-
-  // messageUrl: Summit.messageUrl,
-  // signinUrl: Summit.signinUrl,
-  // redirectForgotPasswordUrl: Summit.redirectForgotPasswordUrl,
-
-  // tenant: { email: Summit.email, 
-  //           tenant_id: Summit.tenant,
-  //           assetTenantId: 'a0642972-e528-4071-b756-e103e85cd9f4' // fixed-asset app
+  // tenant: { 
+  //   email: Formloco.email, 
+  //   tenant_id: Formloco.tenant
   // },
-  // logo: Summit.logo,
-  // idbName: Summit.idbName,
-  // version: Summit.version,
-  // pin: Summit.pin
+  // logo: Formloco.logo,
+  // idbName: Formloco.idbName,
+  // version: Formloco.version,
+  // pin: Formloco.pin
+
+  apiUrl: Summit.apiUrl,
+  authUrl: Summit.authUrl,
+  formUrl: Summit.formUrl,
+  homeUrl: Summit.homeUrl,
+  emailUrl: Summit.emailUrl,
+  assetUrl: Summit.assetUrl,
+  notificationUrl: Summit.notificationUrl,
+
+  messageUrl: Summit.messageUrl,
+  signinUrl: Summit.signinUrl,
+  redirectForgotPasswordUrl: Summit.redirectForgotPasswordUrl,
+
+  tenant: {
+    email: Summit.email,
+    tenant_id: Summit.tenant
+  },
+  logo: Summit.logo,
+  idbName: Summit.idbName,
+  version: Summit.version,
+  pin: Summit.pin
 }
