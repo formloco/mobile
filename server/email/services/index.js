5d4786752be35f5dad0d105825c6c47e01243144
formloco <brock@formloco.com>
2023-02-25 14:05:39 -0700

    added winston log

--- server/email/services/index.js
+++ server/email/services/index.js
@@ -1,12 +1,16 @@
 Mailgun = require('mailgun-js')
 
+const { logger } = require('../../winston')
 const mailgun = new Mailgun({apiKey: process.env.MAILGUN_KEY, domain: process.env.MAILGUN_DOMAIN})
 
 function sendEmail(msg, req, res) {
   mailgun.messages().send(msg).then(resp => {
+    logger.info(resp, 'email')
     res.status(201).send({message: 'Email sent to ' + req.body.email})
+
   })
-  .catch(err => {
+  .catch(msg => {
+    logger.error('Failed to send email.', 'email', 'sendEmail')
     res.status(500).send('Failed to send email.')
   })
 }
